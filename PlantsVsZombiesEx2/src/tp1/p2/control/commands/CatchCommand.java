a54b98564a5c2c86fc24fdcf5dde938daf3de9d5
Melendo <100785500+Melendo@users.noreply.github.com>
2023-05-28 19:24:36 +0200

    Ahora si

---
+++ PlantsVsZombiesEx2/src/tp1/p2/control/commands/CatchCommand.java
@@ -0,0 +1,62 @@
+package tp1.p2.control.commands;
+
+import static tp1.p2.view.Messages.error;
+
+import tp1.p2.control.Command;
+import tp1.p2.control.ExecutionResult;
+import tp1.p2.logic.GameWorld;
+import tp1.p2.view.Messages;
+
+public class CatchCommand extends Command {
+
+	private static boolean caughtSunThisCycle = false;
+
+	private int col;
+
+	private int row;
+
+	public CatchCommand() {
+		caughtSunThisCycle = false;
+	}
+	
+	@Override
+	protected void newCycleStarted() {
+		caughtSunThisCycle = false;
+	}
+
+	private CatchCommand(int col, int row) {
+		this.col = col;
+		this.row = row;
+	}
+
+	@Override
+	protected String getName() {
+		return Messages.COMMAND_CATCH_NAME;
+	}
+
+	@Override
+	protected String getShortcut() {
+		return Messages.COMMAND_CATCH_SHORTCUT;
+	}
+
+	@Override
+	public String getDetails() {
+		return Messages.COMMAND_CATCH_DETAILS;
+	}
+
+	@Override
+	public String getHelp() {
+		return Messages.COMMAND_CATCH_HELP;
+	}
+
+	@Override
+	public ExecutionResult execute(GameWorld game) {
+		// TODO add your code here
+	}
+
+	@Override
+	public Command create(String[] parameters) {
+		// TODO add your code here
+	}
+
+}
